08508d2b7f9b2c97a35c221100bad4deb443a395
Abdul-Rehman <rehmankhan8360@gmail.com>
2026-05-08 16:44:10 +0500

    Update example project

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.wheel_scroll"
+    namespace = "com.example.loading_text_shifter_example"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.wheel_scroll"
+        applicationId = "com.example.loading_text_shifter_example"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- example/android/app/src/main/kotlin/com/example/loading_text_shifter_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/loading_text_shifter_example/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.wheel_scroll
+package com.example.loading_text_shifter_example
 
 import io.flutter.embedding.android.FlutterActivity
 
